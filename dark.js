23b376f7b0cf4ffd90065c29ba17976634b4c64b
Pimous <sandrinekencker@hotmail.com>
2021-01-16 17:58:49 +0100

    amelioration mode dark

--- dark.js
+++ dark.js
@@ -1,11 +1,29 @@
 let input = document.querySelector('input[name="theme"]');
-let body = document.querySelector('body')
+let body = document.querySelector('body');
+let darkmode = localStorage.getItem('darkmode')
+
+const enableDarkmode = () => {
+        body.classList.add('dark')
+        localStorage.setItem('darkmode', 'enabled')
+}
+const disableDarkmode = () => {
+        body.classList.remove('dark')
+        localStorage.setItem('darkmode', null)
+}
+
+if (darkmode === 'enabled') {
+        enableDarkmode();
+}
 
 input.addEventListener('click', () => {
-
-        body.classList.toggle('dark')
-      
-
+        darkmode = localStorage.getItem('darkmode')
+       if (darkmode !== 'enabled') {
+               console.log('activé')
+               enableDarkmode();
+        } else {
+                disableDarkmode();
+                console.log('desactivé')
+       }
 })
 
 
